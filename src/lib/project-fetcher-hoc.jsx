66ce8fa0a5c1501d69ccdaf07b184d2a9298b05b
President-of-China <49942345+President-of-China@users.noreply.github.com>
2024-10-07 01:58:15 +0800

    Update project-fetcher-hoc.jsx

--- src/lib/project-fetcher-hoc.jsx
+++ src/lib/project-fetcher-hoc.jsx
@@ -198,7 +198,7 @@ const ProjectFetcherHOC = function (WrappedComponent) {
         vm: PropTypes.instanceOf(VM)
     };
     ProjectFetcherComponent.defaultProps = {
-        assetHost: 'https://assets.scratch.mit.edu',
+        assetHost: 'https://github.com/open-scratch/scratch-asset-utils/tree/master/scratch3/',
         projectHost: 'https://projects.scratch.mit.edu'
     };
 
